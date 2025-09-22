bb3bf1380458af3b5073b024daab52082b1d7133
Kittu616 <kshitijrphutane@gmail.com>
2025-09-22 21:36:22 +0530

    test cases

--- reduction.py
+++ reduction.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Load your trained model
-model = tf.keras.models.load_model("trained_plant_disease_model_plantvillage.keras")
+model = tf.keras.models.load_model("trained_plant_disease_model.keras")
 
 # Convert to TFLite with quantization
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
@@ -9,7 +9,7 @@ converter.optimizations = [tf.lite.Optimize.DEFAULT]  # enables weight quantizat
 tflite_model = converter.convert()
 
 # Save compressed model
-with open("trained_plant_disease_model_plantvillage.tflite", "wb") as f:
+with open("trained_plant_disease_model.tflite", "wb") as f:
     f.write(tflite_model)
 
 print("✅ Model converted to TFLite and saved.")
